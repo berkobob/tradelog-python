a3174b363dfaa443c195df71b122d09cd12b2f67
Antoine Lever <antoine@antoines-mbp.lan>
2020-02-14 17:23:31 +0000

    Still working on env

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask
-from dotenv import load_dotenv
 
-load_dotenv()
 app = Flask(__name__)
-app.config.from_envvar('APP_CONFIG_FILE')
+app.config.from_object('config.'+app.config['ENV'])
 
-print(app.debug)
+print('*** ENV ***', app.config['ENV'])
+print('** TEST **', app.config['TESTING'])
+print('* DEBUG *', app.config['DEBUG'])
 
 
 @app.route('/')
--- src/db.py
+++ src/db.py
@@ -1,12 +1,1 @@
 from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
-
-load_dotenv('.env.test')
-print(os.getenv('NAME'))
-
-# print(os.environ)
-
-# for value in os.environ:
-#     print(value, ':\t', os.getenv(value))
-
