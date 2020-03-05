96285493174df53d918d99676ba35cf4f00b2bbb
Antoine Lever <antoine@antoines-mbp.lan>
2020-03-05 19:10:06 +0000

    add config classes and mongo atlas

--- src/app.py
+++ src/app.py
@@ -4,11 +4,6 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config.'+app.config['ENV'])
 
-print('*** ENV ***', app.config['ENV'])
-print('** TEST **', app.config['TESTING'])
-print('* DEBUG *', app.config['DEBUG'])
-
-
 @app.route('/')
 def index():
     return "Coming soon"
--- src/db.py
+++ src/db.py
@@ -1,1 +1,9 @@
 from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://tradelog:tradelog@cluster0-4ov7h.mongodb.net/test?retryWrites=true&w=majority')
+db = client.test
+raw = db.raw
+print(raw.find_one())
+
+for trade in raw.find():
+    print(trade)
