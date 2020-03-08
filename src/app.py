547948dbd9312bf780d0c831556c52934d9f7553
BerkoBob <alever@omniis.com>
2020-03-08 18:08:13 +0000

    Use environment variables

--- src/app.py
+++ src/app.py
@@ -9,7 +9,15 @@ app.register_blueprint(web)
 try:
     app.config.from_object('config.'+app.config['ENV'])
 except:
+    pass
+
+if 'DB_URL' not in app.config.keys():
     app.config['DB_URL'] = environ.get('DB_URL')
+
+if app.config['SECRET_KEY'] is None:
     app.config['SECRET_KEY'] = urandom(24)
+
+print('DB_URL:', app.config['DB_URL'])
+print('SECRET_KEY', app.config['SECRET_KEY'])
 
 DB.connect(app.config['DB_URL'], app.config['ENV'])
